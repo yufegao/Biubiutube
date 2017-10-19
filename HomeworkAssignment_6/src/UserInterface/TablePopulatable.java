4f9f2ad387cb5b62d729bfe4e0b80560951f5ab8
Zhognjie He <he.zho@husky.neu.edu>
2017-10-19 00:36:16 -0400

    add UI interface

---
+++ HomeworkAssignment_6/src/UserInterface/TablePopulatable.java
@@ -0,0 +1,46 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package UserInterface;
+
+import java.awt.Component;
+import javax.swing.JOptionPane;
+import javax.swing.JTable;
+import javax.swing.table.DefaultTableModel;
+
+/**
+ *
+ * @author hezj
+ */
+public interface TablePopulatable<Element> {
+    JTable getTable();
+
+    default void populateTable(Iterable<Element> elements) {
+        JTable table = getTable();
+        DefaultTableModel dtm = (DefaultTableModel) table.getModel();
+        dtm.setRowCount(0);
+
+        elements.forEach(e -> dtm.addRow(populateRow(e)));
+    }
+
+    Object[] populateRow(Element element);
+
+    default Object getSelected() {
+        JTable table = getTable();
+        int selectedCount = table.getSelectedRowCount();
+        if (selectedCount > 1) {
+            JOptionPane.showMessageDialog((Component) this, "Please select no more than 1 row.");
+            return null;
+        }
+        if (selectedCount < 1) {
+            JOptionPane.showMessageDialog((Component) this, "Please select 1 row.");
+            return null;
+        }
+
+        return table.getValueAt(table.getSelectedRow(), 0);
+    }
+
+    void populateTable();
+}
