cef709a1478be88e2bcf7ba47e244d7f92f1570f
Xu Han <798133953@qq.com>
2017-11-14 10:27:34 -0500

    Roles done

---
+++ FinalProject/src/biz/role/Role.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package biz.role;
+
+import javax.swing.JPanel;
+
+/**
+ *
+ * @author 79813
+ */
+public abstract class Role {
+    public enum RoleType{
+        
+    }
+    public abstract JPanel createWorkArea(JPanel parent);
+}
---
+++ FinalProject/src/biz/role/producerRole/AdvertisementProducerRole.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package biz.role.producerRole;
+
+import biz.role.Role;
+import javax.swing.JPanel;
+
+/**
+ *
+ * @author 79813
+ */
+public class AdvertisementProducerRole extends Role{
+
+    @Override
+    public JPanel createWorkArea(JPanel parent) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
