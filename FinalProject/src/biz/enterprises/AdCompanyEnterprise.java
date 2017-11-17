62ffe5fa5757d54de02032d4e41a2b35be5abc72
Xu Han <798133953@qq.com>
2017-11-17 17:06:48 -0500

    This commit for save the change in adEnterprise

--- FinalProject/src/biz/enterprises/AdCompanyEnterprise.java
+++ FinalProject/src/biz/enterprises/AdCompanyEnterprise.java
@@ -1,9 +1,19 @@
 package biz.enterprises;
 
 import biz.nw.Network;
+import biz.org.adc.AdevertiseAccountingOrganization;
+import biz.org.adc.AdvertiseAdminOrganization;
+import biz.org.adc.AdvertiseProduceOrganization;
+import biz.org.adc.AdvertiseProduceOrganizationCatalog;
+import biz.org.adc.AdvertiseSupervisorOrganization;
 
 public class AdCompanyEnterprise extends Enterprise {
     // TODO: org and orgCatalog here
+    private AdevertiseAccountingOrganization aAccount;
+    private AdvertiseAdminOrganization aAdmin;
+    private AdvertiseProduceOrganization aProduce;
+    private AdvertiseProduceOrganizationCatalog aPCatalog;
+    private AdvertiseSupervisorOrganization aSupervisor;
     public AdCompanyEnterprise(String name, Network network) {
         super(name, network);
     }
