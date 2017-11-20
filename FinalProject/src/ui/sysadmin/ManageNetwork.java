9d106693ff4d19f7048ea4b63119ce2099f8a05b
GAO YUFEI <gao.yu@husky.neu.edu>
2017-11-20 11:18:58 -0500

    Rename Sysadmin

--- FinalProject/src/ui/sysadmin/ManageNetwork.java
+++ FinalProject/src/ui/sysadmin/ManageNetwork.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package ui.alladmins.sysadmins;
+package ui.sysadmin;
 
 import biz.EcoSystem;
 import biz.nw.Network;
