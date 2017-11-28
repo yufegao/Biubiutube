a9e35756e1c33d348b0029559d1296ed2d439609
GAO YUFEI <gao.yu@husky.neu.edu>
2017-11-27 22:46:03 -0500

    Manage Function 50% finished

--- FinalProject/src/ui/network/commonadm/ManageOrganization/ManageOrganization.java
+++ FinalProject/src/ui/network/commonadm/ManageOrganization/ManageOrganization.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package ui.network.commonadm.ManageAccount;
+package ui.network.commonadm.ManageOrganization;
 
 import biz.account.Account;
 import biz.enterprises.AdCompanyEnterprise;
